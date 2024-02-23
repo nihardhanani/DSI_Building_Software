3480d8e54513770cb0b645ae545ee6bb1b1a04b1
nihardhanani <nihardhanani28@gmail.com>
2024-02-22 23:54:06 -0500

    Issue 1 path error correction. Another try

--- Analysis.py
+++ Analysis.py
@@ -7,10 +7,9 @@ import statistics
 import pandas as pd
 
 class Analysis():
-    # config_path = ['configuration/system_config.yml', 'configuration/user_config.yml']
 
     def __init__(self, analysis_config: str) -> None:
-        CONFIG_PATHS = ['configs\system_config.yml', 'configs\user_config.yml']
+        CONFIG_PATHS = [r'configs\system_config.yml', r'configs\user_config.yml']
 
         # add the analysis config to the list of paths to load
         paths = CONFIG_PATHS + [analysis_config]
